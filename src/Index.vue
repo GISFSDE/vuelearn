857f5872056394b01e9804c7f9a74d97f51cf12c
MrDemonlxl <714416426@qq.com>
2022-06-01 11:26:21 +0800

    VSCode代码模板，Elementui

---
+++ src/Index.vue
@@ -0,0 +1,103 @@
+<template>
+  <el-container>
+    <el-header>GISER HOME</el-header>
+    <el-main
+      ><el-row :gutter="20">
+        <el-col :span="6"
+          ><div class="grid-content bg-purple">LEAFLET</div></el-col
+        >
+        <el-col :span="6"
+          ><div class="grid-content bg-purple">MAPBOX</div></el-col
+        >
+        <el-col :span="6"
+          ><div class="grid-content bg-purple">CESIUM</div></el-col
+        >
+        <el-col :span="6"
+          ><div class="grid-content bg-purple">OTHERS</div></el-col
+        >
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"
+          ><div class="grid-content bg-purple"></div
+        ></el-col> </el-row
+    ></el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  margin-top: 5%;
+  border-radius: 4px;
+  min-height: 360px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -11,5 +13,6 @@ new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  render: h => h(App)
 })
